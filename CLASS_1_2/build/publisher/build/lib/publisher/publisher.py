381cf7436459d24351734d64bbba37f09bdba63e
yanlechuan <2264026818@qq.com>
2024-11-12 01:29:39 +0800

    Add files via upload

---
+++ CLASS_1_2/build/publisher/build/lib/publisher/publisher.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class PublisherNode(Node):
+    def __init__(self):
+        super().__init__('publisher_node')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Welcome to RM！"
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    publisher_node = PublisherNode()
+    rclpy.spin(publisher_node)
+    publisher_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ CLASS_1_2/install/listener/lib/python3.10/site-packages/listener/listener.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class ListenerNode(Node):
+    def __init__(self):
+        super().__init__('listener_node')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    listener_node = ListenerNode()
+    rclpy.spin(listener_node)
+    listener_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
